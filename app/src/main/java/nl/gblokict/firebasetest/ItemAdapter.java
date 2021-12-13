ad1cd91a2a945c9f61f032ac12c549dab70dbd73
GBlok02 <61229884+GBlok02@users.noreply.github.com>
2021-12-13 08:24:07 +0100

    Initial commit

---
+++ app/src/main/java/nl/gblokict/firebasetest/ItemAdapter.java
@@ -0,0 +1,53 @@
+package nl.gblokict.firebasetest;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemAdapter extends FirebaseRecyclerAdapter<
+        Woord, ItemAdapter.ItemViewholder> {
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public ItemAdapter(@NonNull FirebaseRecyclerOptions<Woord> options) {
+        super(options);
+    }
+    @Override
+    protected void onBindViewHolder(@NonNull ItemViewholder holder, int position, @NonNull Woord model) {
+        holder.tvWoordned.setText(model.getWoordned());
+        holder.tvWoordamz.setText(model.getWoordamz());
+    }
+    
+    @NonNull
+    @Override
+    public ItemAdapter.ItemViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.woord_layout, parent, false);
+        return new ItemAdapter.ItemViewholder(view);
+
+    }
+    public class ItemViewholder extends RecyclerView.ViewHolder {
+
+        TextView tvWoordned, tvWoordamz;
+        ImageView ivWoord;
+        
+        public ItemViewholder(@NonNull View itemView) {
+            super(itemView);
+            tvWoordned = itemView.findViewById(R.id.tvWoordned2);
+            tvWoordamz = itemView.findViewById(R.id.tvWoordamz2);
+            ivWoord = itemView.findViewById(R.id.ivWoord2);
+            
+            
+        }
+    }
+}
---
+++ app/src/main/java/nl/gblokict/firebasetest/MainActivity2.java
@@ -0,0 +1,52 @@
+package nl.gblokict.firebasetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity2 extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    ItemAdapter adapter;
+    DatabaseReference algemeen, woorden;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        // reference naar de data
+        woorden = FirebaseDatabase.getInstance("https://fir-test-c1b38-default-rtdb.europe-west1.firebasedatabase.app/").getReference("woorden");
+
+// klaarzetten van de recyclerview
+        recyclerView = findViewById(R.id.recycler1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // bouwen van de dataquery
+        FirebaseRecyclerOptions<Woord> options
+                = new FirebaseRecyclerOptions.Builder<Woord>()
+                .setQuery(woorden, Woord.class)
+                .build();
+
+        // bouwen van de adapter met query
+        adapter = new ItemAdapter(options);
+
+        // koppelen van de adapter
+        recyclerView.setAdapter(adapter);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
